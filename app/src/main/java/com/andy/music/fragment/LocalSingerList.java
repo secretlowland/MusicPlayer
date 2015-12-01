45a9c03df51ea9703f5addc14847be06245de070
andy <andy821280045@gmail.com>
2015-12-01 21:44:10 +0800

    optimize the LoacalSingerList

--- app/src/main/java/com/andy/music/fragment/LocalSingerList.java
+++ app/src/main/java/com/andy/music/fragment/LocalSingerList.java
@@ -67,8 +67,8 @@ public class LocalSingerList extends ListFragment {
                         return "#";
                     }
                 })
+                .sortKeys(new StringComparator())
                 .build();
-        secAdapter.setKeySorting(SectionedListAdapter.Sorting.InputOrder);
         return secAdapter;
     }
 
@@ -139,17 +139,6 @@ public class LocalSingerList extends ListFragment {
 
                 }
                 cursor.close();
-
-                // 根据名字的首字母对列表进行排序
-                Collections.sort(data, new Comparator<HashMap<String, Object>>() {
-                    @Override
-                    public int compare(HashMap<String, Object> lhs, HashMap<String, Object> rhs) {
-                        String ln = (String) lhs.get("name");
-                        String rn = (String) rhs.get("name");
-                        StringComparator comparator = new StringComparator();
-                        return comparator.compare(ln, rn);
-                    }
-                });
 
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
